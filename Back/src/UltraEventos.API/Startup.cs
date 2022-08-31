f5af23d4d270b15fdbc2fb833fdb621736784281
Mateus1110 <tabaldimateus1110@gmail.com>
2022-08-31 16:30:10 -0300

    angular init

--- Back/src/UltraEventos.API/Startup.cs
+++ Back/src/UltraEventos.API/Startup.cs
@@ -32,6 +32,7 @@ namespace UltraEventos.API
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "UltraEventos.API", Version = "v1" });
@@ -53,6 +54,10 @@ namespace UltraEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(cors => cors.AllowAnyHeader()
+                                    .AllowAnyMethod()
+                                    .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
