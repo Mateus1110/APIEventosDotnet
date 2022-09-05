c21db59ca9965b5770dbc8e22cf7a95c9d64b29f
Mateus1110 <tabaldimateus1110@gmail.com>
2022-09-05 13:16:48 -0300

    navbar e eventos

--- Front/UltraEventos-App/src/app/eventos/eventos.component.ts
+++ Front/UltraEventos-App/src/app/eventos/eventos.component.ts
@@ -8,25 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  private _filtro: string = '';
+  imgWidth = 80;
+  imgHeigth = 50;
+  showImage = true;
+
+  public get filtro(): string {
+    return this._filtro;
+  }
+
+  public set filtro(value: string) {
+    this._filtro = value;
+    this.eventosFiltrados = this._filtro ? this.filtrarEventos(this._filtro) : this.eventos
+  }
+
+  filtrarEventos(filter: string): any {
+    filter = filter.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filter) !== -1 ||
+                       evento.local.toLocaleLowerCase().indexOf(filter) !== -1
+    )
+  }
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
   }
 
+  showImagem() {
+    this.showImage = !this.showImage
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/Evento').subscribe({
-       next: (v) => this.eventos = v,
+       next: (v) => {this.eventos = v;
+                     this.eventosFiltrados = this.eventos},
        error: console.error}
     );
   }
-
-  // public getEventos(): void {
-  //   this.http.get('https://localhost:5001/api/Evento').subscribe(
-  //     response => this.eventos = response,
-  //     error => console.log(error)
-  //   );
-  // }
-
 }
